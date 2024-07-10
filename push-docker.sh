d5ca85a800742aaf21afb72be60ebd55028ab763
normausome <normausome@protonmail.com>
2024-07-10 15:36:39 -0400

    fix: uses arm for docker build because running on raspberry pi

--- push-docker.sh
+++ push-docker.sh
@@ -2,7 +2,7 @@
 
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 763831530139.dkr.ecr.us-east-2.amazonaws.com
 
-docker build --platform linux/amd64 -t normanau .
+docker build --platform linux/arm64 -t normanau .
 
 docker tag normanau:latest  763831530139.dkr.ecr.us-east-2.amazonaws.com/normanau:latest
 
