60b20f78f46869553732ec81fef61d60ea78b2e7
normausome <normausome@protonmail.com>
2024-07-12 12:37:29 -0400

    thought this would fix Next.js image optimization issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  /**
+   * doesn't work because cloudflare doesn't support optimizing next/images
+   */
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "normanau.com",
+  //     },
+  //   ],
+  // },
   output: "standalone",
 };
 
